4b9c0a6922ba702d6e0e0b245247200ad8325fc9
Diogo <dapereira@inf.ufsm.br>
2024-07-19 17:40:00 -0300

    hotfix: entry points

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
     ],
     entry_points={
         'console_scripts': [
-            'zorgen-admin=zorgen.zorgen_admin:main',
+            'zorgen-admin=commands.zorgen_admin:main',
         ],
     },
 )
