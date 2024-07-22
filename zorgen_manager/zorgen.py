7a4957778177e23d69d52af6d23eb24507552c71
Diogo <dapereira@inf.ufsm.br>
2024-07-22 13:09:57 -0300

    hotfix: manager

--- zorgen_manager/zorgen.py
+++ zorgen_manager/zorgen.py
@@ -52,13 +52,13 @@ def create_app_directory(app_name):
 
     pycache_dir = os.path.join(destination_dir, '__pycache__')
     if os.path.exists(pycache_dir):
-        print(f"Deleting __pycache__ directory at {pycache_dir}")
+        # print(f"Deleting __pycache__ directory at {pycache_dir}")
         shutil.rmtree(pycache_dir)
 
         for root, dirs, files in os.walk(destination_dir, topdown=False):
             if '__pycache__' in dirs:
                 pycache_dir = os.path.join(root, '__pycache__')
-                print(f"Deleting __pycache__ directory at {pycache_dir}")
+                # print(f"Deleting __pycache__ directory at {pycache_dir}")
                 shutil.rmtree(pycache_dir)
 
 def update_apps_file(app_dir, app_name):
