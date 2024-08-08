edddce04577348a7f57a91874766a6eeb8f5fe1d
Diogo <dapereira@inf.ufsm.br>
2024-08-08 17:00:39 -0300

    raw: requirements to lib

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 PACKAGE_DATA = list()
 setup(
     name="zorgen-manager",
-    version="0.0.3",
+    version="0.0.5",
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
@@ -18,7 +18,12 @@ setup(
         "Programming Language :: Python :: 3.10",
     ],
     install_requires=[
-        "Django"
+        "Django==4.2.13",
+        "python-decouple==3.8",
+        "django-extensions==3.2.3",
+        "requests==2.32.3",
+        "psycopg2==2.9.9",
+        "sentry-sdk==2.11.0",
     ],
     entry_points={
         'console_scripts': [
